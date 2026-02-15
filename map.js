9516597317b4c5745d43550d9fb0e085aa0c37cc
BOOBESH776 <cboobesh776@gmail.com>
2026-02-15 14:03:28 +0530

    update map.js to enhance array manipulation examples and clarify comments

--- map.js
+++ map.js
@@ -1,5 +1,18 @@
 let fruit = ['apple', 'banana', 'orange'];
 fruit.push("grape");
-fruit.shift("gh");
-// fruit.shift("kiwi");
- console.log(fruit); // Output: ['apple', 'banana', 'orange']
+ console.log(fruit); // Output: ['apple', 'banana', 'orange', 'grape']
+
+fruit.pop();
+console.log(fruit); // Output: ['apple', 'banana', 'orange']
+
+// it can be modify the array at the end but it can not modify the array at the beginning or in the middle
+
+
+
+fruit.unshift("grape");
+console.log(fruit); // Output: ['grape', 'apple', 'banana', 'orange']
+
+fruit.shift("grapes");
+console.log(fruit); // Output: ['apple', 'banana', 'orange']
+
+// it can modify the array at the beginning but it can not modify the array at the end or in the middle
