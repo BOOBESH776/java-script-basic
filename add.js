f0ec02ba6eaf1cb929fe43fc4dde84492a0941c6
BOOBESH776 <cboobesh776@gmail.com>
2026-02-16 17:15:18 +0530

    add add.js to implement basic arithmetic and greeting functions

---
+++ add.js
@@ -0,0 +1,25 @@
+
+var mul = (a,b) =>{
+    return a*b;
+}
+console.log(mul(4,4));
+
+let add = (a,b) => a+b;
+
+console.log(add(3,4));
+
+function age(){
+    let age = 20;
+    if(age>=18){
+        console.log("eligiable")
+    }
+    else{
+        console.log("not")
+    }
+}
+age();
+
+function greet(name){
+    console.log("hello "+name);
+}
+greet("kishore");
