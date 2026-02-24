93efbd16a2730b62055b446138905b79e2316b1f
BOOBESH776 <cboobesh776@gmail.com>
2026-02-24 17:56:58 +0530

    Ordering food at a restaurant

--- async/index.js
+++ async/index.js
@@ -1,2 +1,12 @@
 //Ordering food at a restaurant.
 
+function orderfood(){
+    console.log("order dosa");
+}setInterval(orderfood, 5000);
+
+dosome();
+function dosome(){
+    console.log("do some work");
+}
+
+orderfood();
