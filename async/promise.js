ccfba8f92b514bf7efeccfff7d4737bd42fb6ace
BOOBESH776 <cboobesh776@gmail.com>
2026-02-28 17:17:21 +0530

    make a command line to understand code

--- async/promise.js
+++ async/promise.js
@@ -1,31 +1,33 @@
 let ticket = new Promise((resolve,reject) =>{
     let booked = false;
     if(booked)
-        resolve()
+        resolve() //if booked is true then it will go to then block
     else
-        reject()
+        reject() //if booked is false then it will go to catch block
 })
 
-.then(success)
-.catch(error)
+.then(success) //true
+.catch(error)  //false
 
 function success(){
-    console.log("Ticket Booked");
+    console.log("Ticket Booked"); //if booked is true then it will print this
 }   
 
 function error(){
-    console.log("Ticket Not Booked");
+    console.log("Ticket Not Booked"); //if booked is false then it will print this
 }
 
 
-let busticket = new Promise((resolve,reject)=>{
-    let busbooked = true;
+
+let busticket = new Promise((resolve,reject)=>{ //creating a new promise for bus ticket booking
+    let busbooked = true; //you can change this value to false to see the catch block in action
     if(busbooked){
-        resolve()
+        resolve() //if busbooked is true then it will go to then block
     }
     else{
-        reject()
+        reject() //if busbooked is false then it will go to catch block
     }
 })
 
-.then(() => console.log("Bus Ticket Booked")).catch(()=> console.log("Bus Ticket Not Booked"))
+.then(() => console.log("Bus Ticket Booked"))  //true
+.catch(()=> console.log("Bus Ticket Not Booked"))  //false
