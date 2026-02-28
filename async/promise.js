33a4038505aa71321b6cbb8b40a551f1de3f1be6
BOOBESH776 <cboobesh776@gmail.com>
2026-02-28 17:15:01 +0530

    busticket booking

--- async/promise.js
+++ async/promise.js
@@ -1,17 +1,31 @@
 let ticket = new Promise((resolve,reject) =>{
-    let booked = true;
+    let booked = false;
     if(booked)
         resolve()
     else
         reject()
 })
 
-.then(success).catch(error)
+.then(success)
+.catch(error)
 
 function success(){
-    console.log("Ticket Booked")
+    console.log("Ticket Booked");
 }   
 
 function error(){
-    console.log("Ticket Not Booked")
+    console.log("Ticket Not Booked");
 }
+
+
+let busticket = new Promise((resolve,reject)=>{
+    let busbooked = true;
+    if(busbooked){
+        resolve()
+    }
+    else{
+        reject()
+    }
+})
+
+.then(() => console.log("Bus Ticket Booked")).catch(()=> console.log("Bus Ticket Not Booked"))
