30687a038d7ba0b1bbc854ad661142ee7f5eb6a0
BOOBESH776 <cboobesh776@gmail.com>
2026-03-13 19:30:37 +0530

    successfully practice async and await first concept

--- async/ticket.js
+++ async/ticket.js
@@ -1,17 +1,22 @@
 let ticket = new Promise((resolve, reject) => {
     let book = true
-    if(book){
-        console.log("bought a Book");
+    if (book) {
+        setTimeout(resolve,3000,"bought a Book");
     }
-    else{
-        console.log("Not Buy");
+    else {
+        reject("Not Buy");
     }
 })
 
-async function status(){
-    console.log("look the which book want to buy");
-
-    let res = await ticket
-    console.log(res);
-    console.log("going to home");
+async function asyncstatus() {
+    try {
+        console.log("look the which book want to buy");
+        let res = await ticket
+        console.log(res);
+        console.log("going to home");
+    }
+    catch (err) {
+        console.log(err);
+    }
 }
+asyncstatus()
