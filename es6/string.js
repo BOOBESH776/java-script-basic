556c30c7b1d82df02c1f1d4f7976ef16e24e1f76
BOOBESH776 <cboobesh776@gmail.com>
2026-05-04 16:26:40 +0530

    string concept

---
+++ es6/string.js
@@ -0,0 +1,13 @@
+
+let name  = "bob";
+let age = 30;
+
+console.log(`My name is ${name} and i am ${age} years old.`);
+
+let a =10 , b =20;
+console.log(`The sum of ${a} and ${b} is ${a+b}.`);
+
+function greet(name) {
+    console.log(`Hello, ${name.toUpperCase()}!`);
+}
+greet("Alice");
